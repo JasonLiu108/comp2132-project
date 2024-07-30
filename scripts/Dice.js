1df42d6cad9ae08e5c32a0193eb9ed6923a13de0
JasonLiu108 <jasonliu@duck.com>
2024-07-30 04:53:44 -0700

    Update Dice class

---
+++ scripts/Dice.js
@@ -0,0 +1,24 @@
+const imagePath      = "dice-images/";
+const imageExtension = ".jpg";
+
+const minDiceValue = 1;
+const maxDiceValue = 6;
+
+class Dice {
+    #value;
+    #color;
+
+    constructor(value, color) {
+        this.#value = value;
+        this.#color = color;
+    }
+
+    roll() {
+        this.value = Math.ceil(Math.random() * maxDiceValue);
+        return this.value;
+    }
+
+    getImagePath() {
+        return `dice-images/${this.color}/${this.color}-dice-${this.value}.png`;
+    }
+}
